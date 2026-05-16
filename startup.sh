880a3cbc0af4423dc156d93cd6319ec249c12184
ElyseA <127885186+p3rf3ctblu3@users.noreply.github.com>
2026-05-16 16:05:45 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-python -m streamlit run /home/site/wwwroot/main.py --server.port 8000 --server.address 0.0.0.0
+python -m streamlit run /home/site/wwwroot/main.py --server.port 8080 --server.address 0.0.0.0
